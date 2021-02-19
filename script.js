3968c6749a6be7624ad8e03f85b3a7d23b7f52e1
kimnopal <75764266+kimnopal@users.noreply.github.com>
2021-02-19 17:41:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onscroll = function() {
+    navSticky()
+};
+var navbar = document.getElementById('navbar');
+var sticky = navbar.offsetTop;
+function navSticky() {
+    if (window.pageYOffset > 0) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+}
+
+
+const links = document.querySelectorAll('.nav-links li a');
+
+for( const link of links ) {
+    link.addEventListener('click', clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+
+    document.querySelector(href).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
